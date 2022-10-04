627bb633e59017f6fce8b5dfe92b8f7a8976e42f
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-04 12:47:40 +0300

    Задача002

---
+++ classwork_2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая выводит случайное
+трёхзначное число и удаляет вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98 */
+
+void AnswerToTask(){
+
+    int newNumeric = (new Random()).Next(100,1000);
+    Console.WriteLine($"Base numeric: {newNumeric}");    
+    string numericAsString = newNumeric.ToString();
+    int result = int.Parse(numericAsString[0].ToString() + numericAsString[2].ToString());
+    Console.WriteLine($"Result numeric: {result}");
+
+}
+
+AnswerToTask();
