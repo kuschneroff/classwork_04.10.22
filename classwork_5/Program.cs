af3205ca46973006c070d140ed4ce515cadacaf5
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-04 12:48:33 +0300

    Задача005

---
+++ classwork_5/Program.cs
@@ -0,0 +1,26 @@
+﻿/*16. Напишите программу, 
+которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+5, 25  ->  да
+-4, 16  ->  да
+25, 5  ->  да
+8,9  ->  нет
+*/
+
+void GetResult(int firstNumeric, int secondNumeric){
+
+
+    if(firstNumeric * firstNumeric == secondNumeric){
+        Console.WriteLine($"Numeric {secondNumeric} = {firstNumeric}^2");
+        return;
+    }
+
+    if(secondNumeric * secondNumeric == firstNumeric){
+        Console.WriteLine($"Numeric {firstNumeric} = {secondNumeric}^2");
+        return;
+    }
+        
+    Console.WriteLine($"It's so bad result =(");
+
+}
+
+GetResult(16, 4);
