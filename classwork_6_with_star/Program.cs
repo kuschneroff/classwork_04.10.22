d0f3c511bd9cbf11b6a8450226482ae060b465e9
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-04 12:49:20 +0300

    Задача006 со звездочкой

---
+++ classwork_6_with_star/Program.cs
@@ -0,0 +1,30 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        isCorrect = int.TryParse(Console.ReadLine(), out result);
+
+        if(!isCorrect)
+          Console.WriteLine("Ввели не число, введите корректное число");
+
+        Console.WriteLine();
+    }
+
+    return result;
+}
+
+int number = GetNumber("Введите число");
+
+/*
+reference (ссылочные) string class list arrays dictionary...
+value (значимые) - datetime int enum struct decimal long double bool
+
+стек и управляеммая куча
+стек = значимые значения
+управляемой кучи - ссылочные
+*/
