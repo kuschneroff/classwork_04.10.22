22a21c01e2472390ad58cc51edb215ae5a8884b7
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-04 12:48:14 +0300

    Задача004

---
+++ classwork_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, 
+// кратно ли оно одновременно 7 и 23.
+//14 -> нет 
+//46 -> нет 
+//161 -> да
+
+void  GetNumber (int number)
+{
+    int result = number%7;
+    int result1 = number%23;
+    if (result == 0 & result1 == 0)
+    {
+        Console.WriteLine($"{number}-> да");
+    } 
+    else
+    Console.WriteLine($"{number} -> нет");
+}
+GetNumber(161);
